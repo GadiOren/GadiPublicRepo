02e581c280132d891c525152c8da88611a2e47aa
gadior <oren.gadi@gmail.com>
2025-04-06 11:24:39 +0300

    עדכון: אפליקציה עובדת בסביבת פיתוח, מוכנה לפריסה

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def get_status(processing_id):
     return jsonify({"status": "❌ לא נמצא"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
